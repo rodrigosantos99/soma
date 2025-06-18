72daa4865fc652c85e573c54b0b653e09ea29ac9
rodrigosantos99 <148553044+rodrigosantos99@users.noreply.github.com>
2025-06-17 22:54:08 -0300

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -9,6 +9,10 @@ class AccountsConfig(AppConfig):
         from .models import User
         from .signals import post_save_account_receiver
 
+        # Conecta sinal para criação de estudantes e professores
         post_save.connect(post_save_account_receiver, sender=User)
+
+        # Importa os sinais que escutam post_migrate (executa o código de criação do admin)
+        import accounts.signals  # noqa
 
         return super().ready()
